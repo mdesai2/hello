3aaee7b18641d1aa6e7c06342b7a14c345723786
mdesai2 <54672743+mdesai2@users.noreply.github.com>
2019-08-29 08:07:31 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Maalav Desai and Wiech Nuot
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Maalav and Wiech\n");
+
+  return 0;
+}
